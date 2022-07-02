25763b4720242c0b08241e13615256a34e47421c
Anup <anuppoudel60@gmail.com>
2022-07-02 12:58:33 +1000

    components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Account from "./components/Account/Account";
+import Register from "./components/Register/Register";
 
-function App() {
+const App = () => {
+  const isAuth = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/account" element={isAuth ? <Account /> : <Login />} />
+        <Route path="/register" element={isAuth ? <Account /> : <Register />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
